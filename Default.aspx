2878d1f997f7ad3ef1da687f93622be5cab1cddb
krzys <krzys@DELL_G3>
2023-05-09 22:16:46 +0200

    Dodaj pliki projektów.

---
+++ Default.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab_4.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+  <head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Walidacja</title>
+      <style>
+
+       .glowny_blok {
+        width: 550px;
+        height: auto;
+        border: solid 3px black;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        padding: 5px;     
+      }
+      
+      </style>
+
+  </head>
+
+  <body>
+    <form id="form1" runat="server">
+        <div class="glowny_blok">
+      <div>
+          
+        <p>Nazwisko:
+          
+          <asp:TextBox ID="Nazwisko" runat="server"></asp:TextBox>
+          <asp:RequiredFieldValidator ID="RFV1" runat="server" ControlToValidate="Nazwisko" ErrorMessage="Wymagane pole!" ForeColor="Red"></asp:RequiredFieldValidator>
+        </p>
+
+        <p>Hasło:  
+          <asp:TextBox ID="Hasło" runat="server"></asp:TextBox>
+          <asp:RequiredFieldValidator ID="RFV2" runat="server" ControlToValidate="Hasło" ErrorMessage="Wymagane pole!" Display="Dynamic" ForeColor="Red"></asp:RequiredFieldValidator>
+          <asp:RegularExpressionValidator ID="REV1" runat="server" ControlToValidate="Hasło" ErrorMessage="Hasło musi mieć co najmniej 6 znaków" ForeColor="Red" ValidationExpression="^\w{6,}"></asp:RegularExpressionValidator>
+        </p>
+
+        <p>Potwierdzenie hasła:
+          <asp:TextBox ID="p_Hasło" runat="server"></asp:TextBox>
+          <asp:RequiredFieldValidator ID="RFV3" runat="server" ControlToValidate="p_Hasło" ErrorMessage="Wymagane pole!" Display="Dynamic" ForeColor="Red"></asp:RequiredFieldValidator>
+          <asp:CompareValidator ID="CV1" runat="server" ControlToCompare="Hasło" ControlToValidate="p_Hasło" ErrorMessage="Musi być takie samo jak hasło!" ForeColor="Red"></asp:CompareValidator>
+        </p>
+
+        <p>Wiek:
+          <asp:TextBox ID="Wiek" runat="server"></asp:TextBox>
+          <asp:RangeValidator ID="RV1" runat="server" ControlToValidate="Wiek" ErrorMessage="Mnimum 18 - Maksimum 150" ForeColor="Red" MaximumValue="150" MinimumValue="18" Type="Integer"></asp:RangeValidator>
+        </p> 
+
+        <p>Nr albumu:
+          <asp:TextBox ID="Nr_albumu" runat="server"></asp:TextBox>
+          <asp:RegularExpressionValidator ID="REV2" runat="server" ControlToValidate="Nr_albumu" ErrorMessage="Format: lclclc" ForeColor="Red" ValidationExpression="^[a-zA-Z]\d[a-zA-Z]\d[a-zA-Z]\d$"></asp:RegularExpressionValidator>
+        </p>
+
+        <p>Nr telefonu stacjonarnego:
+          <asp:TextBox ID="nr_telefonu" runat="server"></asp:TextBox>
+          <asp:RegularExpressionValidator ID="REV3" runat="server" ControlToValidate="nr_telefonu" ErrorMessage="Format: (cc) ccc-cc-cc" ForeColor="Red" ValidationExpression="^\(\d{2}\) \d{3}-\d{2}-\d{2}$"></asp:RegularExpressionValidator>
+        </p>
+      </div>
+      <br />
+      <asp:Button ID="Przycisk" runat="server" Text="Wyślij" OnClick="Click" />
+    </form>
+
+
+  </body>
+
+</html>
---
+++ Default.aspx.designer.cs
@@ -0,0 +1,161 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Lab_4
+{
+
+
+    public partial class Default
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka Nazwisko.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Nazwisko;
+
+        /// <summary>
+        /// Kontrolka RFV1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RFV1;
+
+        /// <summary>
+        /// Kontrolka Hasło.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Hasło;
+
+        /// <summary>
+        /// Kontrolka RFV2.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RFV2;
+
+        /// <summary>
+        /// Kontrolka REV1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator REV1;
+
+        /// <summary>
+        /// Kontrolka p_Hasło.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox p_Hasło;
+
+        /// <summary>
+        /// Kontrolka RFV3.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RFV3;
+
+        /// <summary>
+        /// Kontrolka CV1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CV1;
+
+        /// <summary>
+        /// Kontrolka Wiek.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Wiek;
+
+        /// <summary>
+        /// Kontrolka RV1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RV1;
+
+        /// <summary>
+        /// Kontrolka Nr_albumu.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Nr_albumu;
+
+        /// <summary>
+        /// Kontrolka REV2.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator REV2;
+
+        /// <summary>
+        /// Kontrolka nr_telefonu.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nr_telefonu;
+
+        /// <summary>
+        /// Kontrolka REV3.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator REV3;
+
+        /// <summary>
+        /// Kontrolka Przycisk.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Przycisk;
+    }
+}
